e14473ef5d01f41b99518f4df5892b9c19d479c6
NeverL3gend <alejandrogarcicojulun@gmail.com>
2022-03-09 08:13:03 -0500

    new beginning

---
+++ Lecture 1/interpolation.py
@@ -0,0 +1,20 @@
+age = 24
+print("My age is %d years" % age)
+
+major = "years"
+minor = "months"
+print ("my age is %d %s, %d %s" % (age, major, 6, minor)) 
+print ("PI is approximately %f" % (22 / 7)) 
+print ("PI is approximately %60.50f" % (22 / 7))
+
+
+#Tuple is a fixed size list
+
+quantity = 10
+price = 5.0
+total = quantity * price
+tax = total / 5
+ 
+Total = total + tax
+ 
+print(total)
