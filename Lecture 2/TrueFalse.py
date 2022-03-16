2d806f32415d938d25553f9891ff684f9d36014b
NeverL3gend <alejandrogarcicojulun@gmail.com>
2022-03-16 09:38:27 -0400

    commit

--- Lecture 2/TrueFalse.py
+++ Lecture 2/TrueFalse.py
@@ -1,14 +1,25 @@
-# changed the value of "day" from Monday to Saturday
-day = "Friday"
-# No value has been changed
-temperature = 30
-# raining is set to true meaning it is raining
-raining = False
+# # changed the value of "day" from Monday to Saturday
+# day = "Friday"
+# # No value has been changed
+# temperature = 30
+# # raining is set to true meaning it is raining
+# raining = False
 
-# in order to print "go swimming" each of the conditions need to equal true
-# changed and not -> or not
-# 
-if (day == "Saturday" and temperature > 27) or not raining:
-    print ("go swimming")
+# # in order to print "go swimming" each of the conditions need to equal true
+# # changed and not -> or not
+# # 
+# if (day == "Saturday" and temperature > 27) or not raining:
+#     print ("go swimming")
+# else:
+#     print ("Learn Python")
+
+if 0: 
+  print ("true")
 else:
-    print ("Learn Python")
+     print ("False")
+     
+name = input("Please enter your name: ")
+if name:
+    print ("Hello, {}".format(name))
+else:
+    print ("Are you the man with no name?")
---
+++ Lecture 2/checkingIn.py
@@ -0,0 +1,11 @@
+from tkinter import Y
+
+
+parrot = "Norwegian Blue"
+
+letter = input("Enter a character: ")
+
+if letter in parrot:
+    print ("{} is in {}".format(letter,parrot))
+else:
+    print ("I don't need that letter")
