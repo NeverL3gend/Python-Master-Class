41bb62a371f1430fcc98cd05f36e2dd65e136480
NeverL3gend <alejandrogarcicojulun@gmail.com>
2022-05-06 08:56:34 -0400

    starting to learn binary search

---
+++ HighLow.py
@@ -0,0 +1,28 @@
+from pickle import TRUE
+# binary formula: low + (high - low) // 2
+low = 1
+high = 1000
+
+print ("please think of a number between {} and {}".format(low, high))
+input("Press ENTER to start")
+
+guesses = 1
+while TRUE:
+    guess = low + (high - low) // 2
+    high_low = input("my guess is {}. Should I guess higher or lower?"
+        "Enter h or l, or c if my guess was correct"
+        .format(guess).casefold())
+    if high_low == "h":
+        #Guess higher. The low end of the ranfe becomes 1 greater than the guess.
+        low = guess + 1
+    elif high_low == "l":
+        #Guess lower. The high end of the range becomes one less than the guess.
+        high = guess - 1
+    elif high_low == "c":
+        print ("I got it in {} guesses!".format(guesses))
+        break
+    else:
+        print ("Please enter h, l or c")
+    
+    guesses = guesses + 1
+    
