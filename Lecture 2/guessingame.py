4db408747a1e9e40c557d5e4ff7c3c74585ddca0
NeverL3gend <alejandrogarcicojulun@gmail.com>
2022-05-06 08:29:29 -0400

    starting to learn binary search

--- Lecture 2/guessingame.py
+++ Lecture 2/guessingame.py
@@ -1,25 +1,51 @@
-answer = 5 
+#module
+from cgi import print_arguments
+import random 
 
-print ("please guess a number 1 through 10: ")
-guess = int(input())
+highest = 1000
+answer = random.randint(1, highest)
+print (answer) #TODO: remove after testing
+guess = 0 # initialize to any number that doesn't equal the answer
+print ("please guess a number between 1 and {}: ".format(highest))
+
+while guess != answer:
+    guess = int(input())
+    if guess == 0:
+       break 
+    if guess == answer:
+        print ("well done you guessed it")
+        break
+    else:
+        if guess < answer:
+            print ("please guess higher")
+        else:
+            print ("guess lower")
+        # guess = int(input())
+        # if guess == answer:
+        #     print ("well done, you guessed it")
+        # else:
+        #     print ("Sorry, you have not guessed correctly")
+    
+
+
 
 #Challenge
 # change the condition "if guess != answer:" to "if guess == answer:"
 # then change the program to give the correct results
 
 #answer
-if guess == answer:
-    print ("You got it the first time")
-else:    
-    if guess > answer:
-        print ("guess lower")
-    else: #guess must be greater than answer
-        print ("Please guess higher")
-    guess = int (input())
-    if guess  == answer:
-        print ("Well done, you have guess correctly")
-    else:
-        print ("sorry, you have guessed incorrectly")
+# if guess == answer:
+#     print ("You got it the first time")
+# else:    
+#     if guess > answer:
+#         print ("guess lower")
+#     else: #guess must be greater than answer
+#         print ("Please guess higher")
+#     guess = int (input())
+#     if guess  == answer:
+#         print ("Well done, you have guess correctly")
+#     else:
+#         print ("sorry, you have guessed incorrectly")
 # else:
 #     print ("You got it the first time")
 
