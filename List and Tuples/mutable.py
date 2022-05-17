e620a1986e595976866c7a5d7b19d84aea01bc03
NeverL3gend <alejandrogarcicojulun@gmail.com>
2022-05-17 07:59:36 -0400

    forgot to push yesterday

--- List and Tuples/mutable.py
+++ List and Tuples/mutable.py
@@ -12,4 +12,12 @@ print(id(another_list))
 shopping_list += ["cookies"]
 print(shopping_list)
 print(id(shopping_list))
+print(another_list)
 
+a = b = c = d = e = f = another_list
+print(a)
+
+print("adding Cream")
+b.append("cream")
+print(c)
+print(d)
