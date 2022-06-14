4c0ea17139e1e8548aa203b7cbd3c2cf7097415f
NeverL3gend <alejandrogarcicojulun@gmail.com>
2022-06-14 14:37:19 -0400

    commit

--- List and Tuples/buy_computer.py
+++ List and Tuples/buy_computer.py
@@ -1,34 +1,31 @@
+from logging.config import valid_ident
+
+
 available_parts = ["computer",
                    "monitor", 
                    "keyboard", 
-                   "mouse", 
-                   "mouse mat", 
+                   "mouse",  
                    "HDMI",
                    "dvd drive"
                    ]
+# valid_choices = [str(i) for i in range(1, len(available_parts)+1)]
+valid_choices = []
+for i in range (1, len(available_parts)+1):
+    valid_choices.append(str(i))
+print(valid_choices)
 current_choice = "-"
 computer_parts = [] # create an empty list
 
 while current_choice != '0':
-    if current_choice in "123456":
+    if current_choice in valid_choices:
         print("adding {}".format(current_choice))
-        if current_choice == "1":
-            computer_parts.append("computer")
-        elif current_choice =='2':
-            computer_parts.append("monitor")
-        elif current_choice == "3":
-            computer_parts.append("keyboard")
-        elif current_choice =="4":
-            computer_parts.append("mouse")
-        elif current_choice =="5":
-            computer_parts.append("mouse mat")
-        elif current_choice == '6':
-            computer_parts.append("HDMI")
-              
+        index = int(current_choice) - 1
+        chosen_part = available_parts[index]
+        computer_parts.append(chosen_part)
     else: 
         print("Please add options from the list below: ")
-        for part in available_parts:
-            print("{0}: {1}".format(available_parts.index(part)+1, part))
+        for number, parts in enumerate (available_parts):
+            print("{0}: {1}".format(number + 1, parts))
         
     current_choice = input()
 print(computer_parts)
