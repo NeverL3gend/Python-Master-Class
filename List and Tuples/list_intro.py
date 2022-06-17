724b30c283ebaf8565b4a7fe4b1df2e356c8ab0e
NeverL3gend <alejandrogarcicojulun@gmail.com>
2022-06-17 15:33:46 -0400

    commit

--- List and Tuples/list_intro.py
+++ List and Tuples/list_intro.py
@@ -4,3 +4,7 @@ computer_parts = ["computer",
                   "mouse", 
                   "mouse mat"
                   ]
+print (computer_parts)
+
+computer_parts[3] = "trackball"
+print (computer_parts)
