b3b931874156d8d995c56de2ad1de64fb473eec4
NeverL3gend <alejandrogarcicojulun@gmail.com>
2022-07-05 12:36:28 -0400

    commit

--- List and Tuples/number_list.py
+++ List and Tuples/number_list.py
@@ -5,16 +5,3 @@ odd = [1,3,5,7,9]
 numbers = even + odd 
 print (numbers)
 
-sorted_numbers = sorted(numbers)
-print (sorted_numbers)
-print (numbers)
-
-digits = list("432985617")
-print (digits)
-
-# more_numbers = list(numbers)
-# more_numbers = numbers[:]
-more_numbers = numbers.copy()
-print (more_numbers)
-print (numbers is more_numbers)
-print (numbers == more_numbers)
